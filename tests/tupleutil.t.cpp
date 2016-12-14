21aaa8806114165f559cdff1e5b165e2503827c5
Steve Downey <sdowney@gmail.com>
2016-12-13 19:45:01 -0500

    Formatting

--- tests/tupleutil.t.cpp
+++ tests/tupleutil.t.cpp
@@ -10,16 +10,17 @@ TEST(TupleTest, maketuple)
 {
     constexpr std::tuple<int, double, long> t = std::make_tuple(1, 2.3, 1l);
     auto arr = tupleutil::tuple_to_array(t);
-    int i = std::get<int>(arr[0]);
-    //static_assert(i == 1);
+    int  i   = std::get<int>(arr[0]);
+    // static_assert(i == 1);
     EXPECT_EQ(1, i);
 
-    auto transform = tupleutil::tuple_transform(t, [](auto i){return i+1;});
+    auto transform =
+        tupleutil::tuple_transform(t, [](auto i) { return i + 1; });
     tupleutil::print(std::cout, transform);
     std::cout << '\n';
 
     auto t2 = tupleutil::tuple_transform(std::make_tuple(4, 5.0),
-                                            [](auto i){return i+1;});
+                                         [](auto i) { return i + 1; });
     tupleutil::print(std::cout, t2);
     std::cout << '\n';
 }
@@ -30,8 +31,8 @@ TEST(TupleTest, getters)
     auto arr = tupleutil::tuple_getters(t);
 
     auto getter = arr[0];
-    auto v = getter(t);
-    int i = std::get<0>(v);
+    auto v      = getter(t);
+    int  i      = std::get<0>(v);
     EXPECT_EQ(1, i);
 }
 
@@ -40,7 +41,7 @@ TEST(TupleTest, gettersStatic)
     constexpr std::tuple<int, double, long> t = std::make_tuple(1, 2.3, 1l);
 
     auto v = tupleutil::get(0, t);
-    int i = std::get<0>(v);
+    int  i = std::get<0>(v);
     EXPECT_EQ(1, i);
 
     auto v2 = tupleutil::get(1, t);
@@ -51,23 +52,21 @@ TEST(TupleTest, gettersStatic)
     EXPECT_EQ(2.3, d);
 
     constexpr auto t2 = std::make_tuple(2.4, 1l);
-    auto v3 = tupleutil::get(0, t2);
-    double d2 = std::get<double>(v3);
+    auto           v3 = tupleutil::get(0, t2);
+    double         d2 = std::get<double>(v3);
 
     EXPECT_EQ(2.4, d2);
-
 }
 
 TEST(TupleTest, gettersDupTypes)
 {
-    auto t = std::make_tuple(1, 2, 3, 4);
+    auto t   = std::make_tuple(1, 2, 3, 4);
     auto arr = tupleutil::tuple_getters(t);
 
     auto getter = arr[3];
-    auto v = getter(t);
+    auto v      = getter(t);
     EXPECT_EQ(3ul, v.index());
     int i = std::get<3>(v);
     EXPECT_EQ(4, i);
 }
-
 }
